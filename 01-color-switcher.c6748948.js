7f1ae45efee8d8e7bf029e828d35bf418a500371
dmgrabski <132050167+dmgrabski@users.noreply.github.com>
2023-11-13 09:50:54 +0000

    Deploying to gh-pages from @ dmgrabski/goit-js-hw-09@5f37a68222f42482a411498896d58e20a57ad00c 🚀

---
+++ 01-color-switcher.c6748948.js
@@ -0,0 +1,12 @@
+let t;let e=!1;// Znajdowanie przycisków w dokumencie
+const n=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");// Dodanie nasłuchiwania dla przycisku "Start"
+n.addEventListener("click",function(){e||(// Ustawienie flagi i wyłączenie przycisku "Start"
+e=!0,n.disabled=!0,// Ustawienie interwału zmiany koloru co sekundę
+t=setInterval(function(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`},1e3))}),// Dodanie nasłuchiwania dla przycisku "Stop"
+o.addEventListener("click",function(){// Sprawdzenie, czy zmiana koloru jest aktywna
+e&&(// Zatrzymanie zmiany koloru i wyczyszczenie interwału
+clearInterval(t),// Umożliwienie ponownego kliknięcia przycisku "Start"
+n.disabled=!1,// Ustawienie flagi na false
+e=!1)});//# sourceMappingURL=01-color-switcher.c6748948.js.map
+
+//# sourceMappingURL=01-color-switcher.c6748948.js.map
